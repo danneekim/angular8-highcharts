09a712fda01079a6b30106e5e6ed667d7564a598
Dannee Kim <kim.dannee@gmail.com>
2020-02-08 17:48:10 -0500

    Create sticky navigation header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  timestamp = '12:00 AM';
+  profileName = 'Profile'
 
   constructor() { }
 
