87b2a165515e6c5217444c928d776bae39f61ba6
Dannee Kim <kim.dannee@gmail.com>
2020-02-09 14:24:46 -0500

    Wiring up routes for LoginComponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,11 @@ import { Routes, RouterModule } from '@angular/router';
 
 // Import Components for Routes:
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent},
+  { path: 'login', component: LoginComponent}
 ];
 
 @NgModule({
