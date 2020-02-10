48d1a5bf01fddd1a1580ad5c5393b9b23957b626
Dannee Kim <kim.dannee@gmail.com>
2020-02-10 00:38:55 -0500

    Adding User Service Logic for app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { first } from 'rxjs/operators';
+
+import { User } from './_models';
+import { UserService, AuthenticationService } from './_services';
 
 @Component({
   selector: 'app-root',
@@ -6,8 +11,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  currentUser: User;
   title = 'angular8-highcharts';
   timestamp = '12:00 AM';
   profileName = 'Profile';
-  
+
+  constructor(
+    private userService: UserService,
+    private router: Router,
+    private authenticationService: AuthenticationService
+  ) {
+  }
+
+  ngOnInit() {
+    this.authenticationService.currentUser.subscribe(x => this.currentUser = x);
+  }
+
+  logout() {
+    this.authenticationService.logout();
+    this.router.navigate(['/login']);
+  }
 }
