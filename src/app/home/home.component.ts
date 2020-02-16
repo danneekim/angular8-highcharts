85a5a085e241b74ac4de364977dec004a6a125d8
Dannee Kim <kim.dannee@gmail.com>
2020-02-15 22:11:22 -0500

    wiring up sample line graph on fetch market data from API

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-
+import * as Highcharts from 'highcharts/highstock';
 
 import { User } from '../_models';
 import { 
@@ -10,6 +10,16 @@ import {
   LoaderService
 } from '../_services';
 
+declare var require: any;
+let Boost = require('highcharts/modules/boost');
+let noData = require('highcharts/modules/no-data-to-display');
+let More = require('highcharts/highcharts-more');
+
+Boost(Highcharts);
+noData(Highcharts);
+More(Highcharts);
+noData(Highcharts);
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -18,7 +28,69 @@ import {
 
 export class HomeComponent implements OnInit {
   currentUser: User;
-  users: User[] = [];
+  graphGenerated: Boolean = false;
+  users: User[] = [];  public options: any = {
+    chart: {
+      type: 'scatter',
+      height: 700
+    },
+    title: {
+      text: 'Line Graph'
+    },
+    credits: {
+      enabled: false
+    },
+    tooltip: {
+      formatter: function () {
+        return 'x: ' + Highcharts.dateFormat('%e %b %y %H:%M:%S', this.x) +
+          'y: ' + this.y.toFixed(2);
+      }
+    },
+    xAxis: {
+      type: 'datetime',
+      labels: {
+        formatter: function () {
+          return Highcharts.dateFormat('%e %b %y', this.value);
+        }
+      }
+    },
+    series: [
+      {
+        name: 'Normal',
+        turboThreshold: 500000,
+        data: [[new Date('2018-01-25 18:38:31').getTime(), 2]]
+      },
+      {
+        name: 'Abnormal',
+        turboThreshold: 500000,
+        data: [[new Date('2018-02-05 18:38:31').getTime(), 7]]
+      }
+    ]
+
+    // rangeSelector: {
+    //   selected: 2
+    // },
+
+    // title: {
+    //   text: 'AAPL Stock Price'
+    // },
+
+    // series: [{
+    //   type: 'ohlc',
+    //   name: 'AAPL Stock Price',
+    //   data: this.marketData,
+    //   dataGrouping: {
+    //     units: [[
+    //       'week', // unit name
+    //       [1] // allowed multiples
+    //     ], [
+    //       'month',
+    //       [1, 2, 3, 4, 6]
+    //     ]]
+    //   }
+    // }]
+
+  }
 
   constructor(
     private authenticationService: AuthenticationService,
@@ -30,13 +102,22 @@ export class HomeComponent implements OnInit {
     this.currentUser = JSON.parse(localStorage.getItem('currentUser'));
   }
 
+  
+
   ngOnInit() {
 
   }
 
   fetchData(apiJson){
+  
     this.marketDataService.getMarketData(apiJson).subscribe(data => {
       console.log(data)
+      if (data){
+        this.graphGenerated = true;
+        if (data && this.graphGenerated === true){
+          Highcharts.chart('graphContainer', this.options);
+        }
+      }
     })
   }
 
