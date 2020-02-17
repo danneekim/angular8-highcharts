9e19d19a173a1a26f4963f6e4524f8404ca28d0f
Dannee Kim <kim.dannee@gmail.com>
2020-02-16 20:11:19 -0500

    Creating scatter graph with dummy data and configure date start

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,7 +7,7 @@ import {
   UserService,
   AuthenticationService,
   MarketDataService,
-  LoaderService, 
+  LoaderService,
   AlertService
 } from '../_services';
 
@@ -29,46 +29,45 @@ noData(Highcharts);
 
 export class HomeComponent implements OnInit {
   currentUser: User;
-  users: User[] = []; 
+  users: User[] = [];
   graphGenerated: Boolean = false;
   public options: any = {
-    chart: {
-      type: 'scatter',
-      height: 700
-    },
-    title: {
-      text: 'Line Graph'
-    },
-    credits: {
-      enabled: false
-    },
-    tooltip: {
-      formatter: function () {
-        return 'x: ' + Highcharts.dateFormat('%e %b %y %H:%M:%S', this.x) +
-          'y: ' + this.y.toFixed(2);
-      }
-    },
-    xAxis: {
-      type: 'datetime',
-      labels: {
-        formatter: function () {
-          return Highcharts.dateFormat('%e %b %y', this.value);
-        }
-      }
-    },
-    series: [
-      {
-        name: 'Normal',
-        turboThreshold: 500000,
-        data: [[new Date('2018-01-25 18:38:31').getTime(), 2]]
-      },
-      {
-        name: 'Abnormal',
-        turboThreshold: 500000,
-        data: [[new Date('2018-02-05 18:38:31').getTime(), 7]]
-      }
-    ]
-
+    // chart: {
+    //   type: 'scatter',
+    //   height: 700
+    // },
+    // title: {
+    //   text: 'Line Graph'
+    // },
+    // credits: {
+    //   enabled: false
+    // },
+    // tooltip: {
+    //   formatter: function () {
+    //     return 'x: ' + Highcharts.dateFormat('%e %b %y %H:%M:%S', this.x) +
+    //       'y: ' + this.y.toFixed(2);
+    //   }
+    // },
+    // xAxis: {
+    //   type: 'datetime',
+    //   labels: {
+    //     formatter: function () {
+    //       return Highcharts.dateFormat('%e %b %y', this.value);
+    //     }
+    //   }
+    // },
+    // series: [
+    //   {
+    //     name: 'Normal',
+    //     turboThreshold: 500000,
+    //     data: [[new Date('2018-01-25 18:38:31').getTime(), 2]]
+    //   },
+    //   {
+    //     name: 'Abnormal',
+    //     turboThreshold: 500000,
+    //     data: [[new Date('2018-02-05 18:38:31').getTime(), 7]]
+    //   }
+    // ]
   }
 
   constructor(
@@ -89,12 +88,48 @@ export class HomeComponent implements OnInit {
     this.alertService.destroy();
     this.marketDataService.getMarketData(apiJson).subscribe(
       data => {
-        console.log(data)
-        if (data) {
-          this.graphGenerated = true;
-          if (data && this.graphGenerated === true) {
-            Highcharts.chart('graphContainer', this.options);
-          }
+        let marketData;
+        marketData = data;
+        let pointStartDate = '';
+        let startYear = 0;
+        let startMonth = 0;
+        let startDay = 0;
+        pointStartDate = marketData.dataset.oldest_available_date.replace(/-/g, '/');
+        startYear = parseFloat(pointStartDate.substring(0, 4));
+        startMonth = parseFloat(pointStartDate.substring(5, 7)) - 1;
+        startDay = parseFloat(pointStartDate.substring(8.9));
+
+        this.options = {
+          rangeSelector: {
+            selected: 2
+          },
+          title: {
+            text: marketData.dataset.name
+          },
+          xAxis: {
+            type: 'datetime'
+          },
+          plotOptions: {
+            series: {
+              pointStart: Date.UTC(startYear, startMonth, startDay),
+              // pointStart: Date.UTC(2010, 0, 1),
+              pointInterval: 24 * 3600 * 1000 // one day
+            }
+          },
+          // series: [{
+          //   type: 'ohlc',
+          //   name: 'AAPL Stock Price',
+          //   data: newOHLCdataArray,
+          // }]
+          series: [{
+            data: [29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4]
+          }, {
+            data: [144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4, 29.9, 71.5, 106.4, 129.2]
+          }]
+        }
+        this.graphGenerated = true;
+        if (this.options != {} && this.graphGenerated === true) {
+          Highcharts.chart('graphContainer', this.options);
         }
       },
       error => {
